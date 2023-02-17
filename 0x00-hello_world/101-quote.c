02cdcb7b7b60a2a91c8548be334ed2879ef47f73
kelue <austincmoon5@gmail.com>
2023-02-17 15:05:15 +0100

    task 7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the main of the program
+ *
+ * Return: always 1
+ */
+
+int main(void)
+{
+	const char msg[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+    	write(2, msg, sizeof(msg) - 1);
+    	return (1);
+}
