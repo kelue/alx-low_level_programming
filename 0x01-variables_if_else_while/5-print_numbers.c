14e345cb81a86190a041ab6f55833462cf44ed06
kelue <austincmoon5@gmail.com>
2023-02-18 20:57:40 +0100

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - main function for task
+*
+* Return: always 0
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
