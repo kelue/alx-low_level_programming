e4b962ee3a4de6278b5075d22c0872ba7e6290b5
kelue <austincmoon5@gmail.com>
2023-02-28 14:43:35 +0100

    task 2 correction

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
 * _strlen - function that determines the length of a string
-* @s: pointer to the first char in the string 
+* @s: pointer to the first char in the string
 *
 * Return: length of string i
 */
@@ -11,7 +11,6 @@ int _strlen(char *s)
 {
 	int i = 1;
 
-	
 	do {
 		i++;
 		s = s + 1;
