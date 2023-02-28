d865b07ffd7424c27a08f64bef4147b742ccf822
kelue <austincmoon5@gmail.com>
2023-02-28 22:41:16 +0100

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,17 +13,24 @@ void puts_half(char *str)
 	int length = 0;
 
 	for (i = 0; str[i] != '\0'; i++)
-		length++;
+	{
+				length++;
+
+	}
 	if (length % 2 == 0)
 	{
 		for  (i = length / 2; str[i] != '\0'; i++);
+		{
 			_putchar(str[i]);
+		}
 		_putchar('\n');
 	}
 	else if (length / 2 != 0)
 	{
 		for  (i = (length - 1) / 2; str[i] != '\0'; i++);
+		{
 			_putchar(str[i]);
+		}
 		_putchar('\n');
 	}
 }
